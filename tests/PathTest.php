45fba090e8561112fa11e5110014e46b08a6ae0c
Marek Sterzik <marek.sterzik@gmail.com>
2026-03-10 17:04:30 +0100

    path test

--- tests/PathTest.php
+++ tests/PathTest.php
@@ -2,22 +2,28 @@
 
 namespace Tests\Suite;
 
+use Sterzik\DI\Path;
 use Tests\Helper;
 use Tests\Path\UnixPath;
 use Tests\Path\WindowsPath;
 
 class PathTest extends AbstractTestCase
 {
-    public function testAbsolutePathUnix()
+    public function testAbsolutePathUnix(): void
     {
         $this->assertSame("/root/test/x", UnixPath::resolve("test/x"));
         $this->assertSame("/test/x", UnixPath::resolve("/test/x"));
     }
 
-    public function testAbsolutePathWindows()
+    public function testAbsolutePathWindows(): void
     {
         $this->assertSame("C:\\root\\test/x", WindowsPath::resolve("test/x"));
         $this->assertSame("\\test/x", WindowsPath::resolve("\\test/x"));
         $this->assertSame("c:\\test/x", WindowsPath::resolve("c:\\test/x"));
+    }
+
+    public function testRootPathDetection(): void
+    {
+        $this->assertSame(dirname(__DIR__), Path::getRoot());
     }
 }
